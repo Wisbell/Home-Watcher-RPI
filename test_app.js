d1ad7cebc118e7c9740551cecb3a9fb29f75ef89
Wisbell <westleyisbell@gmail.com>
2017-05-12 16:24:14 -0500

    Wrapped sending picture to AWS functionality in a promise

--- test_app.js
+++ test_app.js
@@ -76,12 +76,40 @@ board.on("ready", function(){
         console.log('filePath in readPicPromise', filePath)
 
         return new Promise( (resolve, reject) => {
-          fs.readFile(filePath, (err, data) => {
+          fs.readFile(filePath, (err, dataBuffer) => {
             if(err) throw err;
             else {
               console.log('Done reading picture')
-              resolve( { data, filePath })
+              resolve( { dataBuffer, filePath })
             }
+          })
+        })
+
+      }
+
+      let sendPictureToAWS = ( {dataBuffer, filePath} ) => {
+        console.log('filePath in readPicPromise', filePath)
+
+        return new Promise( (resolve, reject) => {
+          s3.createBucket({Bucket: myBucket}, function(err) {
+
+            if (err) {
+               console.log(err);
+            } else {
+
+              params = {Bucket: myBucket, ACL: "public-read", Key: filePath, Body: dataBuffer}
+
+              s3.upload(params, function(err, data){
+                if (err) {
+                  console.log(err)
+                } else {
+                  console.log(`Successfully uploaded data to ${myBucket}/${filePath}`);
+                  console.log("data", data)
+                  resolve(data)
+                }
+              })
+            }
+
           })
         })
 
@@ -90,74 +118,22 @@ board.on("ready", function(){
       //Begin Promise Chain
       takePicture()
       .then( (filePath) => {
-        fs.readFile(filePath, (err, data) => {
-          if(err) throw err;
-          else {
-            console.log('Done reading picture')
-            return { data, filePath }
-          }
+
+        readPictureFile(filePath)
+        .then( ({dataBuffer, filePath}) => {
+
+            sendPictureToAWS()
+
+        })
+        .then( (data) => {
+          console.log('send data to mongo db')
+          console.log("data", data)
+        })
+        .then( () => {
+          console.log('delete the picture file')
+          // after the picture file is deleted reset the proccessingImage flag var
         })
       })
-
-      .then( ({data, filePath}) => {
-        console.log("my stuff")
-        console.log("data", data)
-        console.log("filePath", filePath)
-      })
-
-
-
-
-      // readPictureFile(filePath)
-        // .then( ({data, filePath}) => {
-
-        //     let stuff
-
-        //     s3.createBucket({Bucket: myBucket}, function(err) {
-
-        //     if (err) {
-        //        console.log(err);
-        //     } else {
-
-        //       params = {Bucket: myBucket, ACL: "public-read", Key: filePath, Body: data}
-
-        //       // s3.putObject(params, function(err, data) {
-
-        //       //   if (err) {
-        //       //     console.log(err)
-        //         // } else {
-        //         //   console.log(`Successfully uploaded data to ${myBucket}/${filePath}`);
-        //         //   //processingImage = false
-        //         // }
-
-        //       // })
-
-        //       s3.upload(params, function(err, data){
-        //         if (err) {
-        //           console.log(err)
-        //         } else {
-        //           console.log(`Successfully uploaded data to ${myBucket}/${filePath}`);
-        //           console.log("data", data)
-        //           stuff = "stuff"
-        //           return data
-        //           //processingImage = false
-        //         }
-        //       })
-        //     }
-
-        //   })
-
-        // })
-        // .then( (data) => {
-        //   console.log('send data to mongo db')
-        //   console.log("data", data)
-        // })
-        // .then( () => {
-        //   console.log('delete the picture file')
-        //   // after the picture file is deleted reset the proccessingImage flag var
-        // })
-
-      // })
     } // Closes if statement for proccessing image
 
   })
