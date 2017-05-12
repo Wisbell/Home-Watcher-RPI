adce567dabc14ebaa65a5d261ff0178bc3cfe2f2
Wisbell <westleyisbell@gmail.com>
2017-05-12 16:43:28 -0500

    Updated promise chain to test on RPI

--- test_app.js
+++ test_app.js
@@ -73,8 +73,6 @@ board.on("ready", function(){
       }
 
       let readPictureFile = (filePath) => {
-        console.log('filePath in readPicPromise', filePath)
-
         return new Promise( (resolve, reject) => {
           fs.readFile(filePath, (err, dataBuffer) => {
             if(err) throw err;
@@ -84,19 +82,20 @@ board.on("ready", function(){
             }
           })
         })
-
       }
 
       let sendPictureToAWS = ( {dataBuffer, filePath} ) => {
-        console.log('filePath in readPicPromise', filePath)
+        console.log('filePath in AWS', filePath)
+        console.log('dataBuffer in AWS', dataBuffer)
 
         return new Promise( (resolve, reject) => {
-          s3.createBucket({Bucket: myBucket}, function(err) {
 
+          s3.createBucket({Bucket: myBucket}, function(err) {
+            console.log('in createBucket')
             if (err) {
                console.log(err);
             } else {
-
+              console.log('in Else')
               params = {Bucket: myBucket, ACL: "public-read", Key: filePath, Body: dataBuffer}
 
               s3.upload(params, function(err, data){
@@ -105,7 +104,8 @@ board.on("ready", function(){
                 } else {
                   console.log(`Successfully uploaded data to ${myBucket}/${filePath}`);
                   console.log("data", data)
-                  resolve(data)
+                  // resolve(data)
+                  resolve(true)
                 }
               })
             }
@@ -117,23 +117,14 @@ board.on("ready", function(){
 
       //Begin Promise Chain
       takePicture()
-      .then( (filePath) => {
-
-        readPictureFile(filePath)
-        .then( ({dataBuffer, filePath}) => {
-
-            sendPictureToAWS()
-
+        .then( (filePath) => {
+          return readPictureFile(filePath)
         })
-        .then( (data) => {
-          console.log('send data to mongo db')
-          console.log("data", data)
+        .then( (dataObject) => {
+          return sendPictureToAWS({dataBuffer, filePath})
         })
-        .then( () => {
-          console.log('delete the picture file')
-          // after the picture file is deleted reset the proccessingImage flag var
-        })
-      })
+
+
     } // Closes if statement for proccessing image
 
   })
