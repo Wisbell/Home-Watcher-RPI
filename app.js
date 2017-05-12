00a6c18ab82453518ce2527dcd11f3b5763c1814
Wisbell <westleyisbell@gmail.com>
2017-05-12 12:19:15 -0500

    Testing using s3.upload instead of s3.putObject in order to get image url

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ board.on("ready", function(){
         })
       }
 
-      let readPicturePromise = (filePath) => {
+      let readPictureFile = (filePath) => {
         console.log('filePath in readPicPromise', filePath)
 
         return new Promise( (resolve, reject) => {
@@ -91,7 +91,7 @@ board.on("ready", function(){
       takePicture()
       .then( (filePath) => {
 
-        readPicturePromise(filePath)
+        readPictureFile(filePath)
         .then( ({data, filePath}) => {
 
             s3.createBucket({Bucket: myBucket}, function(err) {
@@ -100,21 +100,38 @@ board.on("ready", function(){
                console.log(err);
             } else {
 
-              params = {Bucket: myBucket, Key: filePath, Body: data}
+              params = {Bucket: myBucket, ACL: "public-read", Key: filePath, Body: data}
 
-              s3.putObject(params, function(err, data) {
+              // s3.putObject(params, function(err, data) {
 
+              //   if (err) {
+              //     console.log(err)
+                // } else {
+                //   console.log(`Successfully uploaded data to ${myBucket}/${filePath}`);
+                //   //processingImage = false
+                // }
+
+              // })
+
+              s3.upload(params, function(err, data){
                 if (err) {
                   console.log(err)
                 } else {
                   console.log(`Successfully uploaded data to ${myBucket}/${filePath}`);
-                  processingImage = false
+                  console.log("data", data)
+                  //processingImage = false
                 }
-
               })
             }
 
           })
+        })
+        .then( () => {
+          console.log('send data to mongo db')
+        })
+        .then( () => {
+          console.log('delete the picture file')
+          // after the picture file is deleted reset the proccessingImage flag var
         })
       })
     } // Closes if statement for proccessing image
