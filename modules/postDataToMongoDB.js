c57cba8236f0081fd255157028ee7fa46de475ce
Wisbell <westleyisbell@gmail.com>
2017-05-13 13:25:01 -0400

    Post request not posting to DB - testing on other machine

--- modules/postDataToMongoDB.js
+++ modules/postDataToMongoDB.js
@@ -2,7 +2,9 @@ console.log('postDataToMongoDB.js called')
 
 // post data to MongoDB
 
-const request = require('request')
+const { post } = require('request')
+
+let databasePostUrl = 'http://192.168.100.4:8080/api/v1/media/new'
 
 let parseMediaType = (data) => {
   return data.key.split('.')[1]
@@ -13,16 +15,33 @@ let parseDateCreated = (data) => {
                  .split('.')[0]
                  .split('_')
                  .join(' ')
-                 .replace(':', '')
+                 .replace(/:/g, '')
 }
+
+
 
 module.exports.postData = (data) => {
 
   let dataToPost = {
-    dateCreated: parseDateCreated(data),
-    mediaType: parseMediaType(data),
-    url: data.location
+    "dateCreated": parseDateCreated(data),
+    "mediaType": parseMediaType(data),
+    "url": data.Location
   }
 
-  console.log('dataToPost', dataToPost)
+  console.log('dataToPost', JSON.stringify(dataToPost))
+
+
+  post(databasePostUrl, dataToPost, (err) => {
+    if (err) {console.log(err)}
+    else {
+      console.log('post completed')
+    }
+  })
+  
 }
+
+/*
+{"dateCreated":"Sat May 13 2017 132120 GMT-0400 EDT","mediaType":"jpg","url":"https://home-watcher.s3.amazonaws.com/images/Sat_May_13_2017_13%3A21%3A20_GMT-0400_%3AEDT%3A.jpg"}
+post completed
+
+ */
