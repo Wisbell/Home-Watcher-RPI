a7472662b48316c53ea9ae67ef38c690bd879756
Wisbell <westleyisbell@gmail.com>
2017-05-13 09:52:03 -0500

    Added picture process into one main function for better readability

---
+++ startProcess.js
@@ -0,0 +1,35 @@
+let { takePicture } = require('./takePicture')
+let { readPictureFile } = require('./readPictureFile')
+let { sendPictureToAWS } = require('./awsS3')
+
+module.exports.startProcess = () => {
+  // Check to see if an image is currently being processed
+  if (!processingImage) {
+
+    processingImage = true
+
+    //Begin Promise Chain
+
+    // Note - maybe set picture timeout to something other than default - default is 5 seconds
+    takePicture()
+      // Read picture file
+      .then( (filePath) => {
+        return readPictureFile(filePath)
+      })
+      // Send picture to AWS S3
+      .then( (dataObject) => {
+        return sendPictureToAWS(dataObject)
+      })
+      // Send returned URL to MongoDB
+      .then( (data) => {
+        console.log("the data", data)
+      })
+      // Delete picture file on RPI
+
+      // Switch readFile to readStream to avoid this step?
+
+      // reset processingImage flag variable
+
+
+  } // Closes if statement for proccessing image
+}
--- test_app.js
+++ test_app.js
@@ -1,32 +1,17 @@
 // Require environment variables
 require("dotenv").config()
 
-// Necessary Modules
-//const fs = require("fs")
-//const AWS = require("aws-sdk")
 const Five = require("johnny-five")
 const Raspi = require("raspi-io")
 
-let { takePicture } = require('./takePicture')
-let { readPictureFile } = require('./readPictureFile')
-let { sendPictureToAWS } = require('./awsS3')
-
+// let { takePicture } = require('./takePicture')
+// let { readPictureFile } = require('./readPictureFile')
+// let { sendPictureToAWS } = require('./awsS3')
+let { startProccess } = require('./startProcess')
 
 // Set flag variable to prevent overloading the Raspberry Pi
 let processingImage = false;
 
-// Update AWS configuration
-/*
-AWS.config.update({
-  accessKeyId:     process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET,
-  region:          process.env.S3_REGION
-});
-
-let s3 = new AWS.S3()
-
-let myBucket = 'home-watcher';
-*/
 // Set up interface for Raspberry Pi hardware
 const board = new Five.Board({
   io: new Raspi()
@@ -48,65 +33,37 @@ board.on("ready", function(){
   motion.on("motionstart", function(){
     console.log("motion started")
 
-    // Check to see if an image is currently being processed
-    if (!processingImage) {
+    startProcess()
 
-      processingImage = true
-      /*
-      let sendPictureToAWS = ( {dataBuffer, filePath} ) => {
-        console.log('filePath in AWS', filePath)
-        console.log('dataBuffer in AWS', dataBuffer)
+    // // Check to see if an image is currently being processed
+    // if (!processingImage) {
 
-        return new Promise( (resolve, reject) => {
+    //   processingImage = true
 
-          s3.createBucket({Bucket: myBucket}, function(err) {
-            console.log('in createBucket')
-            if (err) {
-               console.log(err);
-            } else {
-              console.log('in Else')
-              params = {Bucket: myBucket, ACL: "public-read", Key: filePath, Body: dataBuffer, ContentType: "image/jpeg"}
+    //   //Begin Promise Chain
 
-              s3.upload(params, function(err, data){
-                if (err) {
-                  console.log(err)
-                } else {
-                  console.log(`Successfully uploaded data to ${myBucket}/${filePath}`);
-                  console.log("data", data)
-                  resolve(data)
-                }
-              })
-            }
+    //   // Note - maybe set picture timeout to something other than default - default is 5 seconds
+    //   takePicture()
+    //     // Read picture file
+    //     .then( (filePath) => {
+    //       return readPictureFile(filePath)
+    //     })
+    //     // Send picture to AWS S3
+    //     .then( (dataObject) => {
+    //       return sendPictureToAWS(dataObject)
+    //     })
+    //     // Send returned URL to MongoDB
+    //     .then( (data) => {
+    //       console.log("the data", data)
+    //     })
+    //     // Delete picture file on RPI
 
-          })
-        })
+    //     // Switch readFile to readStream to avoid this step?
 
-      }
-        */
-      //Begin Promise Chain
-
-      // Note - maybe set picture timeout to something other than default - default is 5 seconds
-      takePicture()
-        // Read picture file
-        .then( (filePath) => {
-          return readPictureFile(filePath)
-        })
-        // Send picture to AWS S3
-        .then( (dataObject) => {
-          return sendPictureToAWS(dataObject)
-        })
-        // Send returned URL to MongoDB
-        .then( (data) => {
-          console.log("the data", data)
-        })
-        // Delete picture file on RPI
-
-        // Switch readFile to readStream to avoid this step?
-
-        // reset processingImage flag variable
+    //     // reset processingImage flag variable
 
 
-    } // Closes if statement for proccessing image
+    // } // Closes if statement for proccessing image
 
   })
 
